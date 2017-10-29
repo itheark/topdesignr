f7739a1a89b38dd1fe4962f36b42b504a3f9df10
Balram Pariyarath <balbbrl@gmail.com>
2017-10-29 21:17:52 +0530

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
- $config['base_url']	= 'http://ec2-13-126-239-80.ap-south-1.compute.amazonaws.com/topdesignr/';
+ $config['base_url']	= 'http://ec2-13-126-239-80.ap-south-1.compute.amazonaws.com/topdesignr/index.php/';
 
 
 
